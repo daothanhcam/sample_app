c0d7849f83b780680b80b641a73b51b5041d0b0e
Dao Cam <daothanhcam@gmail.com>
2014-12-15 14:39:29 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'c17347deaf097345b0337ce936e22f95e0239d719055dc56542183630dac8115587276d023c5a6ea3674920331a72ae2372db9c165a72ec8db5cf48cd3ba36f5'
